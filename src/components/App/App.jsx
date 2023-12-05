c66b61247acde14bffa34098aed83efef66f8554
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-12-05 14:10:18 +0100

    create basic files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+export class App extends Component{
+  state = {
+    contacts: [],
+    filter: ''
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        {/* <ContactForm/> */}
+
+        <h2>Contacts</h2>
+        {/* <Filter/> */}
+        {/* <ContactList/> */}
+      </div>
+    );
+  };
+};
